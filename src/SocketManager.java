6d2c96c6bb89f57ed412daa4150625261b003616
Walkitoff <tylerad83@gmail.com>
2022-09-24 06:00:34 -0700

    ReNamed SocketTest to SocketManager

--- src/SocketManager.java
+++ src/SocketManager.java
@@ -1,6 +1,4 @@
 import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -10,7 +8,7 @@ Enter the command: java –jar <ExecutableJarFileName>.jar.
 Verify the result.
  */
 
-public class SocketTest {
+public class SocketManager {
     public static void main(String[] args) throws Exception {
         InetAddress ip = InetAddress.getLocalHost();
         String myIP;
